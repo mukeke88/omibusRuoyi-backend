5ce50f45084da3f3d06e86fdbb45b327d393a326
mukeke88 <makaikai0628@gmail.com>
2024-12-11 15:05:05 +0800

    Successfully get to /login debug point on the backend

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
@@ -21,7 +21,7 @@ import com.ruoyi.system.service.ISysMenuService;
 
 /**
  * 登录验证
- * 
+ *
  * @author ruoyi
  */
 @RestController
@@ -41,7 +41,7 @@ public class SysLoginController
 
     /**
      * 登录方法
-     * 
+     *
      * @param loginBody 登录信息
      * @return 结果
      */
@@ -58,7 +58,7 @@ public class SysLoginController
 
     /**
      * 获取用户信息
-     * 
+     *
      * @return 用户信息
      */
     @GetMapping("getInfo")
@@ -84,7 +84,7 @@ public class SysLoginController
 
     /**
      * 获取路由信息
-     * 
+     *
      * @return 路由信息
      */
     @GetMapping("getRouters")
